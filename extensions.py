61e855d2a4632f33382982feec97578c05095a12
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-06-06 02:51:53 +0400

    ..
    .

--- extensions.py
+++ extensions.py
@@ -6,10 +6,3 @@ mail = Mail()
 login_manager = LoginManager()
 db = SQLAlchemy()
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "5e5rtyG%RYTGuguu"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-
-
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
