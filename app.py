f16ab4a2d8b5eb3d36588cf5c5c8d908e29345b4
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-06-06 03:22:33 +0400

    ..
    ..

--- app.py
+++ app.py
@@ -28,6 +28,62 @@ app.register_blueprint(main_bp)
 
 with app.app_context():
     db.create_all()
+    
+    from models import Book, User
+    
+    if not Book.query.first():
+        books = [
+            {
+                "image": "itends.jpg",
+                "title": "It Ends With Us",
+                "description": "romance novel that explores the complexities of love, and the impact of difficult choices in a woman's life",
+                "price": "25",
+                "author": "Colleen Hoover",
+            },
+            {
+                "image": "star.jpg",
+                "title": "The Five-Star",
+                "description": "a surprising and captivating story about friendship, love, and self-discovery set on Nantucket.",
+                "price": "30",
+                "author": "Elin Hilderbrand",
+            },
+            {
+                "image": "view.jpg",
+                "title": "You With A View",
+                "description": "Romances of JulyTwo high school enemies must reunite for a road trip inspired by their grandparents’ broken engagement",
+                "price": "20",
+                "author": "Jessica Joyce",
+            },
+            {
+                "image": "crown.jpg",
+                "title": "Ghosted",
+                "description": "waited for a phone call that didn't come. Imagine you meet a man, spend six glorious days together, and fall in love",
+                "price": "20",
+                "author": "Amanda Quain",
+            },
+            {
+                "image": "wife.jpg",
+                "title": "The Hating Game",
+                "description": "an executive assistant, locks horns with her colleague, they find themselves attracted to each other.",
+                "price": "30",
+                "author": "Sally Thorm",
+            },
+            {
+                "image": "tessa.jpg",
+                "title": "Unfortunately Yours",
+                "description": "back in Napa Valley with this hilarious rom-com about a down-on-her-luck heiress who suggests a mutually beneficial marriage",
+                "price": "25",
+                "author": "Tessa Bailey",
+            }
+        ]
+        for book in books:
+            new_book = Book(image=book["image"], title=book["title"], description=book["description"], price=book["price"], author=book["author"])
+            db.session.add(new_book)
+        db.session.commit()
+
+    if not User.query.filter_by(username="admin_user").first():
+        admin_user = User(username="admin_user", password="lizi", role="admin")
+        admin_user.create()
 
 if __name__ == "__main__":
     app.run(debug=True)
