2756e40bc5b43956636dd6203c110e13fee6d831
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-06-06 03:44:31 +0400

    .
    .

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+import socket
 from extensions import db, mail, login_manager
 from routes import main_bp
+
+socket.setdefaulttimeout(5)
 
 app = Flask(__name__)
 
@@ -13,8 +16,9 @@ if database_url and database_url.startswith("postgres://"):
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'super-secret-random-key-here')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465           # 587-ის ნაცვლად
+app.config['MAIL_USE_TLS'] = False      # True-ს ნაცვლად
+app.config['MAIL_USE_SSL'] = True       # აქამდე არ გეწერა
 app.config['MAIL_USERNAME'] = 'gigaurilizi1238@gmail.com'
 app.config['MAIL_PASSWORD'] = 'vhctxocrdrxpujlp'
 
